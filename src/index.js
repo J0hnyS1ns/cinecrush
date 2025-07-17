9880daad6e2947f12591fc836fb56b437f6a8943
Charles <charles.dom@outlook.com>
2025-07-17 14:27:33 -0400

    Les pages fonctionne sans refresh yeah

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './styles/index.scss';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
